3e6a67da75404fe363d3dcaa4fa9c71c6fc69cde
Manuel Bravo <mbravo@gmx.es>
2021-06-25 12:52:26 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace electrodomesticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
